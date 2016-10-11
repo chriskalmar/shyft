0bf54df2f4d9de09e94403aac44444aca3697523
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-11 16:06:35 +0200

    refactored test for better reuse

--- test/core/components.spec.js
+++ test/core/components.spec.js
@@ -7,16 +7,22 @@ const registry = require('../../').registry
 const model = require('../fixtures/models/geo.js')
 
 
+function loadAndRenderCoreComponent(name, model) {
+  let component = registry.getCoreComponent(name)
+  let templateVars = component.processor(model)
+  let template = engine.loadTemplate(component.templatePath)
+
+  return template.renderSync(templateVars).trim()
+}
+
+
 describe('component', function() {
 
   describe('schema', function() {
 
     it('should render a DB schema definition', function() {
 
-      let component = registry.getCoreComponent('schema')
-      let templateVars = component.processor(model)
-      let template = engine.loadTemplate(component.templatePath)
-      let result = template.renderSync(templateVars).trim()
+      let result = loadAndRenderCoreComponent('schema', model)
 
       expect(result).to.equal('CREATE SCHEMA IF NOT EXISTS geo;')
     })
