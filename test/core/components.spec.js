f8062c1794a177049b77709e535397e742c23903
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-11 14:33:43 +0200

    implemented test for schema component rendering

---
+++ test/core/components.spec.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+const engine = require('../../').engine
+const registry = require('../../').registry
+
+const model = require('../fixtures/models/geo.js')
+
+
+describe('component', function() {
+
+  describe('schema', function() {
+
+    it('should render a DB schema definition', function() {
+
+      let component = registry.components.core.schema
+      let templateVars = component.processor(model)
+      let template = engine.loadTemplate(component.templatePath)
+      let result = template.renderSync(templateVars).trim()
+
+      expect(result).to.equal('CREATE SCHEMA IF NOT EXISTS geo;')
+    })
+
+  })
+})
