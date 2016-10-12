341778fecbd076abee59e38bcd798748df394b4f
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-12 08:03:55 +0200

    added more tests for engine

--- test/lib/engine.spec.js
+++ test/lib/engine.spec.js
@@ -12,12 +12,39 @@ const model = require('../fixtures/models/geo.js')
 describe('engine', function() {
 
 
-  it('should render a complete model into SQL code', function() {
+  describe('should render a complete model into SQL code', function() {
 
-    const sqlResult = fs.readFileSync('./test/fixtures/renders/full.sql').toString()
-    const result = engine.generateDatabaseSql([model]).trim()
+    let sqlResult = fs.readFileSync('./test/fixtures/renders/full.sql').toString()
 
-    expect(result).to.equal(sqlResult)
+    it('via callback', function() {
+
+      engine.generateDatabaseSql([model], function(err, result) {
+        expect(result.trim()).to.equal(sqlResult)
+      })
+
+    })
+
+
+
+    it('via return', function() {
+
+      let result = engine.generateDatabaseSql([model]).trim()
+
+      expect(result).to.equal(sqlResult)
+    })
+
+  })
+
+
+
+  it('reject code generation if models is not an array', function() {
+
+    function fn() {
+      engine.generateDatabaseSql(model)
+    }
+
+    expect(fn).to.throw(/models needs to be an array/);
+
   })
 
 
