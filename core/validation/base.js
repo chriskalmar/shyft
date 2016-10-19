ddf93d3dc62f30756c2238d5734b5517f53f1624
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-20 01:43:27 +0200

    new function for generating system name regex patterns

--- core/validation/base.js
+++ core/validation/base.js
@@ -4,10 +4,22 @@
 const DOMAIN_NAME_MAX_LENGTH = 20
 
 const SYSTEM_NAME_REGEX = '[a-z][a-z0-9_]'
-const DOMAIN_NAME_PATTERN = `^${SYSTEM_NAME_REGEX}{0,${DOMAIN_NAME_MAX_LENGTH-1}}$`
+const DOMAIN_NAME_PATTERN = buildSytemNamePattern(1, DOMAIN_NAME_MAX_LENGTH)
 
 
 module.exports = {
+  buildSytemNamePattern
+}
+
+
+
+// returns a regex pattern with a length range definition
+function buildSytemNamePattern (minLength, maxLength) {
+  return `^${SYSTEM_NAME_REGEX}{${minLength-1},${maxLength-1}}$`
+}
+
+
+module.exports.schema = {
 
   title: 'JSON Schema for Shift',
   $schema: 'http://json-schema.org/draft-04/schema#',
