906d7a36c9cef1a1f94702c0c08ba7aa8dda507a
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-20 02:12:56 +0200

    extended json schema for entity definition

--- core/validation/base.js
+++ core/validation/base.js
@@ -2,9 +2,11 @@
 'use strict';
 
 const DOMAIN_NAME_MAX_LENGTH = 20
+const ENTITY_NAME_MAX_LENGTH = 40
 
 const SYSTEM_NAME_REGEX = '[a-z][a-z0-9_]'
 const DOMAIN_NAME_PATTERN = buildSytemNamePattern(1, DOMAIN_NAME_MAX_LENGTH)
+const ENTITY_NAME_PATTERN = buildSytemNamePattern(1, ENTITY_NAME_MAX_LENGTH)
 
 
 module.exports = {
@@ -14,7 +16,7 @@ module.exports = {
 
 
 // returns a regex pattern with a length range definition
-function buildSytemNamePattern (minLength, maxLength) {
+function buildSytemNamePattern(minLength, maxLength) {
   return `^${SYSTEM_NAME_REGEX}{${minLength-1},${maxLength-1}}$`
 }
 
@@ -49,7 +51,38 @@ module.exports.schema = {
     },
     entities: {
       type: 'object',
-      minProperties: 1
+      minProperties: 1,
+      additionalProperties: false,
+      patternProperties: {
+        [ ENTITY_NAME_PATTERN ]: {
+          type: 'object',
+          additionalProperties: false,
+          required: [
+            'description',
+            'attributes'
+          ],
+          properties: {
+            cascadeOnDelete: {
+              type: 'boolean'
+            },
+            description: {
+              $ref: '#/definitions/typeDescription'
+            },
+            attributes: {
+              type: 'object'
+            },
+            indexing: {
+              type: 'object'
+            }
+
+          }
+
+        }
+      }
+    }
+  }
+}
+
     }
   }
 }
