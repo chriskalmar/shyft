ac60e3db367d432a651488cea9c6cd9b9b1f5539
Christian Kalmar <christian.kalmar@gmail.com>
2016-10-27 02:18:19 +0200

    extended tests with new registry functions

--- test/lib/registry.spec.js
+++ test/lib/registry.spec.js
@@ -3,60 +3,96 @@
 
 const registry = require('../../').registry
 
+const model = require('../fixtures/models/geo.js')
 
 
 describe('registry', function() {
 
 
-  it('should store core components', function() {
+  describe('components', function() {
 
-    registry.setCoreComponent('example', function() {}, './test/fixtures/templates/empty.marko')
 
-    expect(registry.components.core).to.have.property('example')
+    it('should store core components', function() {
+
+      registry.setCoreComponent('example', function() {}, './test/fixtures/templates/empty.marko')
+
+      expect(registry.components.core).to.have.property('example')
+    })
+
+
+
+    it('should reject core components with missing or non-function processors', function() {
+
+      function fn() {
+        registry.setCoreComponent('example', null, './test/fixtures/templates/empty.marko')
+      }
+
+      expect(fn).to.throw(/requires processor to be a function/);
+    })
+
+
+
+    it('should reject core components where the template path does not exist', function() {
+
+      function fn() {
+        registry.setCoreComponent('example', function() {}, '/tmp/-no-file-here.marko')
+      }
+
+      expect(fn).to.throw(/could not find template/);
+    })
+
+
+
+
+    it('should return core components upon request', function() {
+
+      let component = registry.getCoreComponent('example')
+
+      expect(component.name).to.equal('example')
+    })
+
+
+
+
+    it('should throw an error if unknown core component is being fetched', function() {
+
+      function fn() {
+        registry.getCoreComponent('this-does-not-exist')
+      }
+
+      expect(fn).to.throw(/unknown core component/);
+    })
+
+
   })
 
 
 
-  it('should reject core components with missing or non-function processors', function() {
 
-    function fn() {
-      registry.setCoreComponent('example', null, './test/fixtures/templates/empty.marko')
-    }
-
-    expect(fn).to.throw(/requires processor to be a function/);
-  })
+  describe('models', function() {
 
 
+    it('should import entity models', function() {
 
-  it('should reject core components where the template path does not exist', function() {
+      registry.importEntityModels(model)
 
-    function fn() {
-      registry.setCoreComponent('example', function() {}, '/tmp/-no-file-here.marko')
-    }
-
-    expect(fn).to.throw(/could not find template/);
-  })
+      expect(registry.components.models).to.have.deep.property('geo.country')
+    })
 
 
 
+    it('should return entity models upon request', function() {
 
-  it('should return core components upon request', function() {
+      let entityModel = registry.getEntityModel('geo', 'country')
 
-    let component = registry.getCoreComponent('example')
-
-    expect(component.name).to.equal('example')
-  })
+      expect(entityModel.domain).to.equal('geo')
+      expect(entityModel.name).to.equal('country')
+    })
 
 
 
 
-  it('should throw an error if unknown core component is being fetched', function() {
 
-    function fn() {
-      registry.getCoreComponent('this-does-not-exist')
-    }
-
-    expect(fn).to.throw(/unknown core component/);
   })
 
 
