01920ef36256e08cf0322153f491e89227ac5647
Chris Kalmar <christian.kalmar@gmail.com>
2017-04-04 18:19:51 +0200

    adapt test to new schema component

--- test/core/components.spec.js
+++ test/core/components.spec.js
@@ -1,25 +1,23 @@
 
-import { engine, registry } from '../../';
+import { registry } from '../../';
 
-import model from '../fixtures/models/simple/geo.js';
+// import model from '../fixtures/models/simple/geo.js';
 
 
 function loadAndRenderCoreComponent(name, theModel) {
   const component = registry.getCoreComponent(name)
-  const templateVars = component.processor(theModel)
-  const template = engine.loadTemplate(component.templatePath)
-
-  return template.renderToString(templateVars).trim()
+  return component.generateSql(theModel).trim()
 }
 
 
-describe('component', () => {
+describe.only('component', () => {
 
   describe('schema', () => {
 
     it('should render a DB schema definition', () => {
 
-      const result = loadAndRenderCoreComponent('schema', model)
+      const schemaNames = [ 'geo' ]
+      const result = loadAndRenderCoreComponent('schema', schemaNames)
 
       expect(result).to.equal('CREATE SCHEMA IF NOT EXISTS geo;')
     })
