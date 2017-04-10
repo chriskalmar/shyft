1a5911fae29c3185d1029c84e417b4dbd7f0e9a0
Chris Kalmar <christian.kalmar@gmail.com>
2017-04-10 02:45:25 +0200

    update sql generation test

--- test/fixtures/renders/full.sql
+++ test/fixtures/renders/full.sql
@@ -6,7 +6,7 @@ SET client_min_messages TO WARNING;
 
     CREATE SCHEMA IF NOT EXISTS geo;
   ;
-create table "geo"."country" ("id" serial primary key, "created_at" timestamptz not null default CURRENT_TIMESTAMP, "updated_at" timestamptz not null default CURRENT_TIMESTAMP, "name" text, "iso_code" text, "continent" text);
+create table "geo"."country" ("id" bigserial primary key, "created_at" timestamptz not null default CURRENT_TIMESTAMP, "updated_at" timestamptz not null default CURRENT_TIMESTAMP, "name" text, "iso_code" text, "continent" text);
 comment on table "geo"."country" is 'A country on our beautiful planet';
 comment on column "geo"."country"."name" is 'The name of the country';
 comment on column "geo"."country"."iso_code" is 'ISO code of the country';
