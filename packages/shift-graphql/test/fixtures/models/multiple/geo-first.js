82c81182ead90d2166207ee601aaa9ed1e553470
Chris Kalmar <christian.kalmar@gmail.com>
2017-04-19 01:36:49 +0200

    implement test for type name generation

---
+++ packages/shift-graphql/test/fixtures/models/multiple/geo-first.js
@@ -0,0 +1,47 @@
+
+export default {
+
+  domain: 'geo',
+
+  entities: [
+
+    {
+      name: 'country',
+      description: 'A country on our beautiful planet',
+
+      attributes: [
+
+        {
+          name: 'name',
+          type: 'string',
+          description: 'The name of the country',
+          minLength: 1
+        },
+
+        {
+          name: 'iso_code',
+          type: 'string',
+          description: 'ISO code of the country',
+          pattern: '^[a-z]+$',
+          minLength: 3,
+          maxLength: 3
+        },
+
+        {
+          name: 'continent',
+          type: 'reference',
+          description: 'The continent where the country is located',
+          target: 'geo::continent'
+        }
+
+      ],
+
+      indexing: {
+        unique: [
+          [ 'name' ]
+        ]
+      }
+
+    }
+  ]
+}
--- packages/shift-graphql/test/lib/util.spec.js
+++ packages/shift-graphql/test/lib/util.spec.js
@@ -1,15 +1,20 @@
 
 import util from '../../lib/util';
+import { engine, registry } from 'shift-engine';
 
+const domainModelsFilePath = __dirname + '/../fixtures/models/multiple/'
 
 describe('util', () => {
 
+  it('should generate a type name from a model', () => {
 
-  it('should generate an entity name from a model', () => {
+    registry.clearAllDomainModel()
+    engine.loadDomainModelsFromFilePath(domainModelsFilePath)
 
-    const result = util.generateEntityName({})
+    const entityModel = registry.getEntityModel('geo', 'country')
+    const result = util.generateTypeName(entityModel)
 
-    expect(result).to.equal('tbd');
+    expect(result).to.equal('geoCountry');
 
   })
 
