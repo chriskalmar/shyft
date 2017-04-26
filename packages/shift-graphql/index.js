accf978e79c36918747a6c375c807f11a5bfb84c
Chris Kalmar <christian.kalmar@gmail.com>
2017-04-26 23:54:36 +0200

    expose util and constants lib

--- packages/shift-graphql/index.js
+++ packages/shift-graphql/index.js
@@ -1,16 +1,22 @@
 
 import schema from './lib/schema';
 import datatype from './lib/datatype';
+import util from './lib/util';
+import constants from './lib/constants';
 
 
 export {
   schema,
   datatype,
+  util,
+  constants,
 }
 
 
 export default {
   schema,
   datatype,
+  util,
+  constants,
 }
 
