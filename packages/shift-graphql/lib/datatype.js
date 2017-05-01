6e2c6596b942b1df6c79f1a1ba749cc0c656fc6d
Chris Kalmar <christian.kalmar@gmail.com>
2017-05-01 12:33:19 +0200

    move custom data types definition into separate folder

--- packages/shift-graphql/lib/datatype.js
+++ packages/shift-graphql/lib/datatype.js
@@ -1,27 +1,12 @@
 
 import {
-  // GraphQLID,
   GraphQLInt,
   GraphQLFloat,
   GraphQLString,
   GraphQLBoolean,
-  GraphQLScalarType,
-  Kind,
 } from 'graphql';
 
-
-export const GraphQLBigInt = new GraphQLScalarType({
-  name: 'BigInt',
-  description:
-    'The `BigInt` scalar type represents a 64-bit integer. As JavaScript ' +
-    'is limited to a precision of 53 bits on integer values, all `BigInt` ' +
-    'values will be output as strings.',
-  serialize: String,
-  parseValue: String,
-  parseLiteral(ast) {
-    return ast.kind === Kind.STRING ? ast.value : null;
-  }
-});
+import GraphQLBigInt from './datatypes/bigint';
 
 
 const dataTypeMap = {
---
+++ packages/shift-graphql/lib/datatypes/bigint.js
@@ -0,0 +1,22 @@
+
+import {
+  GraphQLScalarType,
+  Kind,
+} from 'graphql';
+
+
+export const GraphQLBigInt = new GraphQLScalarType({
+  name: 'BigInt',
+  description:
+    'The `BigInt` scalar type represents a 64-bit integer. As JavaScript ' +
+    'is limited to a precision of 53 bits on integer values, all `BigInt` ' +
+    'values will be output as strings.',
+  serialize: String,
+  parseValue: String,
+  parseLiteral(ast) {
+    return ast.kind === Kind.STRING ? ast.value : null;
+  }
+});
+
+
+export default GraphQLBigInt
