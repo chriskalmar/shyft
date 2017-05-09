b17b2561fd96774b1e83baff421af4e3c39049f2
Chris Kalmar <christian.kalmar@gmail.com>
2017-05-09 02:06:28 +0200

    generate in-memory model based on given schema

--- packages/shift-memory-connector/index.js
+++ packages/shift-memory-connector/index.js
@@ -1,12 +1,16 @@
 
 import StorageTypeMemory from './lib/StorageTypeMemory';
-
+import {
+  generateMemoryDB,
+} from './lib/generator';
 
 export {
   StorageTypeMemory,
+  generateMemoryDB,
 }
 
 
 export default {
   StorageTypeMemory,
+  generateMemoryDB,
 }
---
+++ packages/shift-memory-connector/lib/generator.js
@@ -0,0 +1,78 @@
+
+import { isEntity } from 'shift-engine';
+import _ from 'lodash';
+
+
+export const generateMemoryDB = (schema) => {
+
+  const memoryDB = {}
+
+  _.map(schema.getEntities(), (entity, entityName) => {
+
+    const model = {
+
+      name: entityName,
+      description: entity.description,
+
+      fields: () => {
+        const fields = {}
+
+        _.map(entity.getAttributes(), (attribute) => {
+
+          // skip for computed values
+          if (attribute.resolve) {
+            return
+          }
+
+          memoryDB[ entityName ].fieldTransformerMap[ attribute.gqlFieldName ] = attribute.name
+
+          const field = {
+            description: attribute.description,
+          };
+
+
+          // it's a reference
+          if (isEntity(attribute.type)) {
+
+            const targetEntity = attribute.type
+            const targetEntityName = targetEntity.name
+
+            field.type = memoryDB[ targetEntityName ].model
+          }
+          // it's a regular attribute
+          else {
+            field.type = attribute.type
+          }
+
+          fields[ attribute.name ] = field;
+
+        });
+
+        return fields
+      }
+    }
+
+
+
+    memoryDB[ entityName ] = {
+      data: [],
+      model,
+      fieldTransformerMap: {},
+    }
+
+  })
+
+
+
+  _.map(memoryDB, (entity) => {
+
+    // lazy generate fields
+    entity.model.fields = entity.model.fields()
+
+  })
+
+
+
+  return memoryDB;
+
+}
