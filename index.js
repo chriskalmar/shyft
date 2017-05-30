9b6ab87fc295a6b02325a236db197475c1c067ef
Chris Kalmar <christian.kalmar@gmail.com>
2017-05-30 12:37:06 +0200

    expose generator functions

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 
 import constants from './lib/constants';
 import StorageTypePostgres from './lib/StorageTypePostgres';
-
+import {
+  generateDB,
+  generateData,
+} from './lib/generator';
 
 export {
   constants,
   StorageTypePostgres,
+  generateDB,
+  generateData,
 }
 
 
 export default {
   constants,
   StorageTypePostgres,
+  generateDB,
+  generateData,
 }
