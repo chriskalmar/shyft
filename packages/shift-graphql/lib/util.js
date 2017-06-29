1c560fed2d8079182fc811de444fdd3b192a16b4
Chris Kalmar <christian.kalmar@gmail.com>
2017-06-29 02:30:50 +0200

    add base64 and cursor utils

--- packages/shift-graphql/lib/util.js
+++ packages/shift-graphql/lib/util.js
@@ -43,6 +43,21 @@ export function generateTypeNamePluralPascalCase(name) {
 }
 
 
+export const toBase64 = (value) => new Buffer(value.toString()).toString('base64')
+export const fromBase64 = (value) => new Buffer(value.toString(), 'base64').toString()
+
+
+export const serializeCursor = (cursor) => toBase64( JSON.stringify(cursor) )
+export const deserializeCursor = (cursor) => {
+  try {
+    return JSON.parse( fromBase64(cursor) )
+  }
+  catch(e) {
+    throw new Error('Invalid cursor provided')
+  }
+}
+
+
 export default {
   generateTypeName,
   pascalCase,
@@ -50,4 +65,8 @@ export default {
   generateTypeNamePascalCase,
   generateTypeNamePlural,
   generateTypeNamePluralPascalCase,
+  toBase64,
+  fromBase64,
+  serializeCursor,
+  deserializeCursor,
 }
