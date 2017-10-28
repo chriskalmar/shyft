4e5c0c48cc34d01190faf485c6c3e8c6c61ce124
Chris Kalmar <christian.kalmar@gmail.com>
2017-10-28 22:28:56 +0200

    remove random output mock functions from code coverage

--- packages/shift-engine/lib/datatype/dataTypes.js
+++ packages/shift-engine/lib/datatype/dataTypes.js
@@ -9,7 +9,8 @@ import _ from 'lodash';
 export const DataTypeUserID = new DataTypeUser({
   name: 'DataTypeUserID',
   description: 'Data type representing a reference to a user',
-  mock: () => casual.integer(2^20, 2^51).toString(),
+  mock: /* istanbul ignore next */
+    () => casual.integer(2^20, 2^51).toString(),
 })
 
 
@@ -17,73 +18,83 @@ export const DataTypeUserID = new DataTypeUser({
 export const DataTypeID = new DataType({
   name: 'DataTypeID',
   description: 'Data type representing unique IDs',
-  mock: () => casual.integer(2^20, 2^51).toString(),
+  mock: /* istanbul ignore next */
+    () => casual.integer(2^20, 2^51).toString(),
 })
 
 
 export const DataTypeInteger = new DataType({
   name: 'DataTypeInteger',
   description: 'Data type representing integer values',
-  mock: () => casual.integer(-2^10, 2^10),
+  mock: /* istanbul ignore next */
+    () => casual.integer(-2^10, 2^10),
 })
 
 
 export const DataTypeBigInt = new DataType({
   name: 'DataTypeBigInt',
   description: 'Data type representing big integer values',
-  mock: () => casual.integer(2^20, 2^51).toString(),
+  mock: /* istanbul ignore next */
+    () => casual.integer(2^20, 2^51).toString(),
 })
 
 
 export const DataTypeFloat = new DataType({
   name: 'DataTypeFloat',
   description: 'Data type representing float values',
-  mock: () => casual.double(-2^10, 2^10),
+  mock: /* istanbul ignore next */
+    () => casual.double(-2^10, 2^10),
 })
 
 
 export const DataTypeBoolean = new DataType({
   name: 'DataTypeBoolean',
   description: 'Data type representing boolean values',
-  mock: () => casual.boolean,
+  mock: /* istanbul ignore next */
+    () => casual.boolean,
 })
 
 
 export const DataTypeString = new DataType({
   name: 'DataTypeString',
   description: 'Data type representing text values',
-  mock: () => casual.title,
+  mock: /* istanbul ignore next */
+    () => casual.title,
 })
 
 
 export const DataTypeJson = new DataType({
   name: 'DataTypeJson',
   description: 'Data type representing json objects',
-  mock: randomJson,
+  mock: /* istanbul ignore next */
+    randomJson,
 })
 
 
 export const DataTypeTimestamp = new DataType({
   name: 'DataTypeTimestamp',
   description: 'Data type representing a timestamp',
-  mock: () => new Date(casual.unix_time * 1000),
+  mock: /* istanbul ignore next */
+    () => new Date(casual.unix_time * 1000),
 })
 
 
 export const DataTypeTimestampTz = new DataType({
   name: 'DataTypeTimestampTz',
   description: 'Data type representing a timestamp with time zone information',
-  mock: () => new Date(casual.unix_time * 1000),
+  mock: /* istanbul ignore next */
+    () => new Date(casual.unix_time * 1000),
 })
 
 export const DataTypeDate = new DataType({
   name: 'DataTypeDate',
   description: 'Data type representing a date',
-  mock: () => new Date(casual.unix_time * 1000),
+  mock: /* istanbul ignore next */
+    () => new Date(casual.unix_time * 1000),
 })
 
 
-
+/* istanbul ignore next */
 function randomJson() {
   const ret = {}
   const count = casual.integer(1, 5)
