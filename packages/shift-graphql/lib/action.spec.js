356f607bfbddd909b6ff86fcebd59b76b62cc868
Chris Kalmar <christian.kalmar@gmail.com>
2018-01-02 01:23:56 +0100

    test complex/nested action input types

--- packages/shift-graphql/lib/action.spec.js
+++ packages/shift-graphql/lib/action.spec.js
@@ -3,6 +3,8 @@ import {
   Action,
   DataTypeString,
   DataTypeInteger,
+  buildObjectDataType,
+  buildListDataType,
 } from 'shift-engine';
 
 import {
@@ -80,5 +82,85 @@ describe('action', () => {
     expect(fields).toMatchSnapshot();
   })
 
+
+
+  it('should handle nested input and output definitions', () => {
+
+    const player = buildObjectDataType({
+      attributes: {
+        number: {
+          type: DataTypeInteger,
+          description: 'Number on the shirt',
+          required: true
+        },
+        firstName: {
+          type: DataTypeString,
+          description: 'First name',
+        },
+        lastName: {
+          type: DataTypeString,
+          description: 'Last name',
+          required: true,
+        },
+      },
+    })
+
+    const complexAction = new Action({
+      name: 'buildTeam',
+      description: 'build a team',
+      input: {
+        teamName: {
+          type: DataTypeString,
+          description: 'Name of the team',
+        },
+        players: {
+          type: buildObjectDataType({
+            attributes: {
+              offense: {
+                type: buildListDataType({
+                  itemType: player,
+                }),
+                description: 'Offense players',
+              },
+              defence: {
+                type: buildListDataType({
+                  itemType: player,
+                }),
+                description: 'Defence players',
+              },
+            },
+          }),
+          description: 'List of player',
+        }
+      },
+      output: {
+        luckyNumber: {
+          type: DataTypeInteger,
+          description: 'The perfect lucky number for the given name'
+        }
+      },
+      resolve() { },
+    })
+
+
+    const inputType = generateActionDataInput(complexAction)
+    expect(inputType.name).toEqual('BuildTeamDataInput');
+
+    const inputFields = inputType.getFields()
+    expect(inputFields).toMatchSnapshot();
+
+    const playersInputType = inputFields.players.type
+    const playersInputFields = playersInputType.getFields()
+    expect(playersInputFields).toMatchSnapshot();
+
+
+    const offenseInputType = playersInputFields.offense.type.ofType
+    const defenceInputType = playersInputFields.defence.type.ofType
+    const offenseInputFields = offenseInputType.getFields()
+    const defenceInputFields = defenceInputType.getFields()
+    expect(offenseInputFields).toMatchSnapshot();
+    expect(defenceInputFields).toMatchSnapshot();
+
+  })
 
 })
