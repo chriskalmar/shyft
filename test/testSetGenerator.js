05663d3b5c4db29560ee8f5d63d24ca7698d5c5e
Chris Kalmar <christian.kalmar@gmail.com>
2018-02-14 22:48:58 +0100

    generate test data for board joins, invites and accepted invites

--- test/testSetGenerator.js
+++ test/testSetGenerator.js
@@ -1,8 +1,12 @@
 import casual from 'casual';
-import { generateRows } from './testingData';
+import _ from 'lodash';
+import {
+  generateRows,
+  readRows,
+  writeTestDataFile,
+} from './testingData';
 
 const profileCount = 110
-const boardCount = 50
 
 generateRows(profileCount, 'profiles', () => {
   return [
@@ -11,6 +15,9 @@ generateRows(profileCount, 'profiles', () => {
   ]
 })
 
+
+const boardCount = 50
+
 generateRows(boardCount, 'boards', () => {
   return [
     casual.title,
@@ -18,3 +25,66 @@ generateRows(boardCount, 'boards', () => {
     casual.integer(0, 1),
   ]
 })
+
+
+const inviteCount = 100
+
+const generateInvites = () => {
+  const uniqnessCache = []
+  const boards = readRows('boards')
+  const invites = []
+
+  while (invites.length < inviteCount) {
+    const [name, owner, isPrivate] = _.sample(boards)
+
+    if (isPrivate === '1') {
+      const invitee = casual.integer(1, profileCount)
+      const accept = casual.integer(0, 1)
+
+      if (invitee !== parseInt(owner, 10)) {
+        const row = `${name},${owner},${invitee},${accept}`
+
+        if (!uniqnessCache.includes(row)) {
+          uniqnessCache.push(row)
+          invites.push(row)
+        }
+      }
+    }
+  }
+
+  return invites
+}
+
+writeTestDataFile('invites', generateInvites())
+
+
+const joinCount = 200
+
+const generateJoins = () => {
+  const uniqnessCache = []
+  const boards = readRows('boards')
+  const joins = []
+
+  while (joins.length < joinCount) {
+    const [name, owner, isPrivate] = _.sample(boards)
+
+    if (isPrivate === '0') {
+      const invitee = casual.integer(1, profileCount)
+
+      if (invitee !== parseInt(owner, 10)) {
+        const row = `${name},${invitee}`
+
+        if (!uniqnessCache.includes(row)) {
+          uniqnessCache.push(row)
+          joins.push(row)
+        }
+      }
+    }
+  }
+
+  return joins
+}
+
+writeTestDataFile('joins', generateJoins())
+
+
