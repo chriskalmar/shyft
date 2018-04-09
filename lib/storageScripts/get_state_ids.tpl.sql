eef3d6ad1e968c9e1a55e2381636f161dd1c1d6f
Chris Kalmar <christian.kalmar@gmail.com>
2018-04-09 12:06:51 +0200

    provide postgres functions to get multiple state ids or a full mapping of state names and state IDs

---
+++ lib/storageScripts/get_state_ids.tpl.sql
@@ -0,0 +1,39 @@
+
+CREATE OR REPLACE FUNCTION <%= functionName %>(
+  entity text,
+  state_names text[]
+) RETURNS INTEGER[] AS $$
+DECLARE
+  statesMap JSON;
+  state_name TEXT;
+  state_id INTEGER;
+  result INTEGER[] DEFAULT '{}';
+BEGIN
+
+  -- This is an auto-generated function
+  -- Template (get_state_ids.sql.tpl)
+
+  statesMap := '<%=
+    JSON
+      .stringify(statesMap, null, 2)
+      .split("\n")
+      .join("\n  ")
+  %>';
+
+  IF (statesMap->entity IS NOT NULL) THEN
+    FOREACH state_name IN ARRAY state_names LOOP
+      IF (statesMap->entity->state_name IS NOT NULL) THEN
+        state_id := statesMap->entity->state_name;
+        result := result || state_id;
+      ELSE
+        RAISE EXCEPTION 'Unknown state name used in <%= functionName %>(): %.%', entity, state_name;
+      END IF;
+    END LOOP;
+  ELSE
+    RAISE EXCEPTION 'Unknown entity name used in <%= functionName %>(): %', entity;
+  END IF;
+
+  RETURN result;
+END;
+$$
+LANGUAGE plpgsql STRICT IMMUTABLE;
---
+++ lib/storageScripts/get_state_map.tpl.sql
@@ -0,0 +1,28 @@
+
+CREATE OR REPLACE FUNCTION <%= functionName %>(
+  entity text
+) RETURNS JSON AS $$
+DECLARE
+  statesMap JSON;
+BEGIN
+
+  -- This is an auto-generated function
+  -- Template (get_state_map.sql.tpl)
+
+  statesMap := '<%=
+    JSON
+      .stringify(statesMap, null, 2)
+      .split("\n")
+      .join("\n  ")
+  %>';
+
+  IF (statesMap->entity IS NOT NULL) THEN
+    RETURN statesMap->entity;
+  ELSE
+    RAISE EXCEPTION 'Unknown entity name used in <%= functionName %>(): %', entity;
+  END IF;
+
+  RETURN result;
+END;
+$$
+LANGUAGE plpgsql STRICT IMMUTABLE;
