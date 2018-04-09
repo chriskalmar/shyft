da77228693bd961bd0dbc81c9128eb646ff139d3
Chris Kalmar <christian.kalmar@gmail.com>
2018-04-09 12:05:58 +0200

    install storage scripts and provide postgres function to get state IDs based on entity and state name

---
+++ lib/storageScripts/get_state_id.tpl.sql
@@ -0,0 +1,32 @@
+
+CREATE OR REPLACE FUNCTION <%= functionName %>(
+  entity text,
+  state_name text
+) RETURNS INTEGER AS $$
+DECLARE
+  statesMap JSON;
+BEGIN
+
+  -- This is an auto-generated function
+  -- Template (get_state_id.sql.tpl)
+
+  statesMap := '<%=
+    JSON
+      .stringify(statesMap, null, 2)
+      .split("\n")
+      .join("\n  ")
+  %>';
+
+  IF (statesMap->entity IS NOT NULL) THEN
+    IF (statesMap->entity->state_name IS NOT NULL) THEN
+      RETURN statesMap->entity->state_name;
+    ELSE
+      RAISE EXCEPTION 'Unknown state name used in <%= functionName %>(): %.%', entity, state_name;
+    END IF;
+  ELSE
+    RAISE EXCEPTION 'Unknown entity name used in <%= functionName %>(): %', entity;
+  END IF;
+
+END;
+$$
+LANGUAGE plpgsql STRICT IMMUTABLE;
