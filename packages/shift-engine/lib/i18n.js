62bd7c55721e9a8da9accdc825c1095839a385b6
Chris Kalmar <christian.kalmar@gmail.com>
2018-04-11 17:46:06 +0200

    update i18n mock data generator to use correct storage attribute names

--- packages/shift-engine/lib/i18n.js
+++ packages/shift-engine/lib/i18n.js
@@ -2,18 +2,20 @@
 import { randomJson } from './util';
 import _ from 'lodash';
 
-export const i18nMockGenerator = (entity, name, languages={}) => {
+export const i18nMockGenerator = (entity, name, { dataShaperMap }, languages={}) => {
 
   if (entity) {
     const content = {}
 
     _.map(entity.getAttributes(), ({ type, i18n, mock }, attributeName) => {
+      const storageAttributeName = dataShaperMap[ attributeName ]
+
       if (i18n) {
         if (Math.random() > 0.5) {
           return
         }
 
-        const attributeContent = content[attributeName] = {}
+        const attributeContent = content[ storageAttributeName ] = {}
 
         Object.values(languages).map((languageId, idx) => {
           if (idx === 0 || Math.random() > 0.5) {
