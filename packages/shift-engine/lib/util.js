0c4d7d5fe1c535efd2d535f44985e6aa6ed7929f
Chris Kalmar <christian.kalmar@gmail.com>
2018-05-01 00:14:24 +0200

    add async forEach mapper

--- packages/shift-engine/lib/util.js
+++ packages/shift-engine/lib/util.js
@@ -338,3 +338,11 @@ export const randomJson = () => {
 
   return ret
 }
+
+
+
+export const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[ index ], index, array)
+  }
+}
