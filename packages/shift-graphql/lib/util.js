4dbc05c8ae4ac69ade87c923d1cc6c98a2995779
Chris Kalmar <christian.kalmar@gmail.com>
2018-05-29 22:43:17 +0200

    implement helper functions to translate instances and fall back to originals if requested

--- packages/shift-graphql/lib/util.js
+++ packages/shift-graphql/lib/util.js
@@ -96,6 +96,53 @@ export const addRelayTypePromoterToListFn = (typeName) => {
 
 
 
+export const translateInstance = (entity, instance, { i18nLanguage, i18nLanguageDisableFallback }) => {
+  if (!instance || !i18nLanguage || i18nLanguage === 'default' || !entity.getI18nAttributeNames()) {
+    return instance
+  }
+
+  const attributes = entity.getAttributes()
+  const i18nAttributeNames = entity.getI18nAttributeNames()
+
+  i18nAttributeNames.map(attributeName => {
+
+    const {
+      gqlFieldName,
+      gqlFieldNameI18n,
+    } = attributes[ attributeName ]
+
+    const translation = instance[ gqlFieldNameI18n ]
+      ? instance[ gqlFieldNameI18n ][ i18nLanguage ]
+      : undefined
+
+    instance[ gqlFieldName ] = i18nLanguageDisableFallback
+      ? translation
+      : translation || instance[ gqlFieldName ]
+  })
+
+  return instance
+}
+
+export const translateList = (entity, list, context) => {
+  return list.map(instance => {
+    return translateInstance(entity, instance, context)
+  })
+}
+
+export const translateInstanceFn = (entity, context) => {
+  return (instance) => {
+    return translateInstance(entity, instance, context)
+  }
+}
+
+export const translateListFn = (entity, context) => {
+  return (list) => {
+    return translateList(entity, list, context)
+  }
+}
+
+
+
 export default {
   generateTypeName,
   pascalCase,
