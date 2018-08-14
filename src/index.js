2d8e99312bb92a44e283750a4dc276161f4e20ea
Chris Kalmar <christian.kalmar@gmail.com>
2018-08-14 09:06:54 +0200

    update main exports

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-import constants from './constants';
-import CustomError from './CustomError';
-import Entity, { isEntity } from './entity/Entity';
-import Schema from './schema/Schema';
-import StorageType from './storage/StorageType';
-import StorageDataType from './storage/StorageDataType';
-import ProtocolType from './protocol/ProtocolType';
-import Configuration, { isConfiguration } from './configuration/Configuration';
+import constants from './engine/constants';
+import CustomError from './engine/CustomError';
+import Entity, { isEntity } from './engine/entity/Entity';
+import Schema from './engine/schema/Schema';
+import StorageType from './engine/storage/StorageType';
+import StorageDataType from './engine/storage/StorageDataType';
+import ProtocolType from './engine/protocol/ProtocolType';
+import Configuration, {
+  isConfiguration,
+} from './engine/configuration/Configuration';
 import ProtocolConfiguration, {
   isProtocolConfiguration,
-} from './protocol/ProtocolConfiguration';
+} from './engine/protocol/ProtocolConfiguration';
 import StorageConfiguration, {
   isStorageConfiguration,
-} from './storage/StorageConfiguration';
-import Index, { INDEX_UNIQUE, INDEX_GENERIC } from './index/Index';
+} from './engine/storage/StorageConfiguration';
+import Index, { INDEX_UNIQUE, INDEX_GENERIC } from './engine/index/Index';
 import Action, {
   isAction,
   ACTION_TYPE_MUTATION,
   ACTION_TYPE_QUERY,
-} from './action/Action';
+} from './engine/action/Action';
 import Mutation, {
   MUTATION_TYPE_CREATE,
   MUTATION_TYPE_UPDATE,
   MUTATION_TYPE_DELETE,
-} from './mutation/Mutation';
+} from './engine/mutation/Mutation';
 import Permission, {
   checkPermissionSimple,
   buildPermissionFilter,
   buildActionPermissionFilter,
-} from './permission/Permission';
+} from './engine/permission/Permission';
 
 import {
   passOrThrow,
@@ -41,19 +43,22 @@ import {
   processCursors,
   deleteUndefinedProps,
   isDefined,
-} from './util';
+} from './engine/util';
 
-import { processFilter, convertFilterLevel } from './filter';
+import { processFilter, convertFilterLevel } from './engine/filter';
 
 import {
   fillSystemAttributesDefaultValues,
   fillDefaultValues,
   serializeValues,
-} from './helpers';
+} from './engine/helpers';
 
-import { validateMutationPayload, validateActionPayload } from './validation';
+import {
+  validateMutationPayload,
+  validateActionPayload,
+} from './engine/validation';
 
-import DataType, { isDataType } from './datatype/DataType';
+import DataType, { isDataType } from './engine/datatype/DataType';
 
 import {
   DataTypeUserID,
@@ -69,24 +74,28 @@ import {
   DataTypeDate,
   DataTypeUUID,
   DataTypeI18n,
-} from './datatype/dataTypes';
+} from './engine/datatype/dataTypes';
 
-import DataTypeEnum, { isDataTypeEnum } from './datatype/DataTypeEnum';
-import ComplexDataType, { isComplexDataType } from './datatype/ComplexDataType';
-import DataTypeState, { isDataTypeState } from './datatype/DataTypeState';
+import DataTypeEnum, { isDataTypeEnum } from './engine/datatype/DataTypeEnum';
+import ComplexDataType, {
+  isComplexDataType,
+} from './engine/datatype/ComplexDataType';
+import DataTypeState, {
+  isDataTypeState,
+} from './engine/datatype/DataTypeState';
 
 import ObjectDataType, {
   isObjectDataType,
   buildObjectDataType,
-} from './datatype/ObjectDataType';
+} from './engine/datatype/ObjectDataType';
 
 import ListDataType, {
   isListDataType,
   buildListDataType,
-} from './datatype/ListDataType';
+} from './engine/datatype/ListDataType';
 
-import { Language } from './models/Language';
-import { User } from './models/User';
+import { Language } from './engine/models/Language';
+import { User } from './engine/models/User';
 
 const coreModels = {
   Language,
