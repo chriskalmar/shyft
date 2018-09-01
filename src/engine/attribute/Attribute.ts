ed02df0b716db13c99851c31141ed04c2c60aa23
Chris Kalmar <christian.kalmar@gmail.com>
2018-09-01 13:55:37 +0200

    introduce attribute typings

---
+++ src/engine/attribute/Attribute.ts
@@ -0,0 +1,30 @@
+import { ComplexDataType } from '../datatype/ComplexDataType';
+import { DataType, DataTypeFunction } from '../datatype/DataType';
+import { Entity } from '../entity/Entity';
+
+export type Attribute = {
+  name: string;
+  description: string;
+  type: DataType | ComplexDataType | Entity | DataTypeFunction;
+  required?: boolean;
+  primary?: boolean;
+  unique?: boolean;
+  index?: boolean;
+  resolve?: () => any;
+  defaultValue?: () => any;
+  serialize?: () => any;
+  validate?: () => any;
+  hidden?: boolean;
+  i18n?: boolean;
+  mock?: () => any;
+  mutationInput?: boolean;
+  meta?: any;
+};
+
+export type AttributesMap = {
+  [key: string]: Attribute;
+};
+
+export type AttributesMapFunction = () => AttributesMap;
+
+export type AttributeSetup = {};
