757d17ea4ce0b1fc6c09956e47c323589e677251
Chris Kalmar <christian.kalmar@gmail.com>
2019-03-30 03:19:45 +0100

    update libs and move to babel preset-env

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,11 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
   },
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
   // transformIgnorePatterns: [ '/node_modules/', '^.+\\.js?$' ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|tsx?)$',
   moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
