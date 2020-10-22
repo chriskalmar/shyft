f1d54d6a5968b4345ec3bb13c5c71490605a4eb3
Chris Kalmar <christian.kalmar@gmail.com>
2020-10-22 11:32:58 +0200

    add tests for index name generator

--- src/util.spec.js
+++ src/util.spec.js
@@ -1,4 +1,4 @@
-import { quote } from './util';
+import { generateIndexName, quote, SYSTEM_NAME_MAX_LENGTH } from './util';
 
 describe('util', () => {
   describe('quote', () => {
@@ -18,6 +18,41 @@ describe('util', () => {
       expect(
         quote("someEntity.someAttributeName->'someProp'"),
       ).toMatchSnapshot();
+    });
+  });
+
+  describe('generateIndexName', () => {
+    it('should generate index names', () => {
+      expect(
+        generateIndexName('some_entity', ['first_name', 'last_name']),
+      ).toMatchSnapshot();
+    });
+
+    it('should generate unique index names based on attribute order', () => {
+      expect(
+        generateIndexName('some_entity', ['last_name', 'first_name']),
+      ).toMatchSnapshot();
+    });
+
+    it('should generate index with custom suffix', () => {
+      expect(
+        generateIndexName(
+          'some_entity',
+          ['last_name', 'first_name'],
+          'my_suffix',
+        ),
+      ).toMatchSnapshot();
+    });
+
+    it(`should limit the total index name length to SYSTEM_NAME_MAX_LENGTH (${SYSTEM_NAME_MAX_LENGTH})`, () => {
+      const name = generateIndexName('some_entity', [
+        'first_name',
+        'last_name',
+        'very_long_attribute_name',
+        'even_longer_attribute_name',
+      ]);
+
+      expect(name.length).toEqual(SYSTEM_NAME_MAX_LENGTH);
     });
   });
 });
