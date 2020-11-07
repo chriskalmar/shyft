8454b1452180d7b176fbdcbe9a66355872a836bb
Chris Kalmar <christian.kalmar@gmail.com>
2020-11-07 20:30:30 +0100

    handle escaped commas when loading test data

--- test/testingData.js
+++ test/testingData.js
@@ -4,13 +4,18 @@ const buildArray = (length, value = 1) => {
   return new Array(length).fill(value);
 };
 
-export const readRows = fileName => {
+export const readRows = (fileName) => {
   const filePath = `${__dirname}/data/${fileName}.csv`;
   const content = fs.readFileSync(filePath, 'utf8');
   return content
     .split('\n')
-    .filter(row => row.length)
-    .map(row => row.split(','));
+    .filter((row) => row.length)
+    .map((row) => {
+      // take care of escaped commas '\,'
+      const prep = row.replace(/\\,/g, '|||');
+      const columns = prep.split(',');
+      return columns.map((column) => column.replace(/\|\|\|/g, ','));
+    });
 };
 
 export const writeTestDataFile = (fileName, content) => {
