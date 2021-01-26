6546d21acbd50fdc7407ef937a38e7b7c87a9da2
Chris Kalmar <christian.kalmar@gmail.com>
2021-01-26 03:26:11 +0100

    add graphql queries to instance tests

--- test/instance.spec.ts
+++ test/instance.spec.ts
@@ -1,110 +1,157 @@
 import './setupAndTearDown';
-import { findOne, findOneByValue } from './db';
+import { findOne, findOneByValue, testGraphql } from './db';
 
 import { asAdmin, removeDynamicData, removeListDynamicData } from './testUtils';
 
 import { Profile } from './models/Profile';
 import { Board } from './models/Board';
 import { BoardMember } from './models/BoardMember';
+import { gql } from '../src/graphqlProtocol/util';
 
-describe('postgres', () => {
-  it('should fetch instances by ID', async () => {
-    let profile;
+describe('instance', () => {
+  describe('via connector', () => {
+    it('should fetch instances by ID', async () => {
+      let profile;
 
-    profile = await findOne(Profile, 10, {}, asAdmin());
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+      profile = await findOne(Profile, 10, {}, asAdmin());
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
 
-    profile = await findOne(Profile, 20, {}, asAdmin());
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+      profile = await findOne(Profile, 20, {}, asAdmin());
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
 
-    profile = await findOne(Profile, 30, {}, asAdmin());
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+      profile = await findOne(Profile, 30, {}, asAdmin());
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
 
-    let board;
+      let board;
 
-    board = await findOne(Board, 10, {}, asAdmin());
-    expect(removeDynamicData(Board, board)).toMatchSnapshot();
+      board = await findOne(Board, 10, {}, asAdmin());
+      expect(removeDynamicData(Board, board)).toMatchSnapshot();
 
-    board = await findOne(Board, 20, {}, asAdmin());
-    expect(removeDynamicData(Board, board)).toMatchSnapshot();
+      board = await findOne(Board, 20, {}, asAdmin());
+      expect(removeDynamicData(Board, board)).toMatchSnapshot();
 
-    board = await findOne(Board, 30, {}, asAdmin());
-    expect(removeDynamicData(Board, board)).toMatchSnapshot();
+      board = await findOne(Board, 30, {}, asAdmin());
+      expect(removeDynamicData(Board, board)).toMatchSnapshot();
 
-    let boardMember;
+      let boardMember;
 
-    boardMember = await findOne(BoardMember, 200, {}, asAdmin());
-    expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+      boardMember = await findOne(BoardMember, 200, {}, asAdmin());
+      expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
 
-    boardMember = await findOne(BoardMember, 201, {}, asAdmin());
-    expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+      boardMember = await findOne(BoardMember, 201, {}, asAdmin());
+      expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
 
-    boardMember = await findOne(BoardMember, 202, {}, asAdmin());
-    expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+      boardMember = await findOne(BoardMember, 202, {}, asAdmin());
+      expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+    });
+
+    it('should fetch instances by value', async () => {
+      let profile;
+
+      profile = await findOneByValue(
+        Profile,
+        { username: '----unknown----' },
+        asAdmin(),
+      );
+      expect(profile).toBeUndefined();
+
+      profile = await findOneByValue(
+        Profile,
+        { username: 'katrina560' },
+        asAdmin(),
+      );
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+
+      profile = await findOneByValue(
+        Profile,
+        { username: 'clark218' },
+        asAdmin(),
+      );
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+
+      profile = await findOneByValue(Profile, {}, asAdmin());
+      expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
+
+      let boardMember;
+
+      boardMember = await findOneByValue(
+        BoardMember,
+        { board: 9999 },
+        asAdmin(),
+      );
+      expect(boardMember).toBeUndefined();
+
+      boardMember = await findOneByValue(
+        BoardMember,
+        { board: 9, invitee: 7, inviter: 52 },
+        asAdmin(),
+      );
+      expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+
+      boardMember = await findOneByValue(BoardMember, {}, asAdmin());
+      expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
+    });
+
+    it('should utilize data loader', async () => {
+      const context = asAdmin();
+
+      const result = await Promise.all([
+        findOne(Profile, 10, {}, context),
+        findOne(Profile, 20, {}, context),
+        findOne(Profile, 30, {}, context),
+        findOne(Profile, 20, {}, context),
+        findOne(Profile, 10, {}, context),
+        findOneByValue(Profile, { username: 'katrina560' }, context),
+        findOneByValue(Profile, { username: 'clark218' }, context),
+        findOneByValue(Profile, { username: 'katrina560' }, context),
+      ]);
+
+      expect(removeListDynamicData(Profile, result)).toMatchSnapshot();
+    });
+
+    it('should return null when instance is not found', async () => {
+      const profile = await findOne(Profile, 10000, {}, asAdmin());
+      expect(profile).toBeNull();
+    });
   });
 
-  it('should fetch instances by value', async () => {
-    let profile;
+  describe('via GrahpQL', () => {
+    it('should fetch instances by ID', async () => {
+      const result = await testGraphql(
+        gql`
+          query instances {
+            profileById(id: 10) {
+              id
+              username
+              firstname
+              lastname
+              confirmedAt
+            }
+            boardById(id: 20) {
+              id
+              name
+              owner
+              vip
+              isPrivate
+              metaData {
+                description
+                externalLinks
+              }
+              mods
+            }
+            boardMemberById(id: 200) {
+              id
+              board
+              invitee
+              inviter
+              state
+            }
+          }
+        `,
+        asAdmin(),
+      );
 
-    profile = await findOneByValue(
-      Profile,
-      { username: '----unknown----' },
-      asAdmin(),
-    );
-    expect(profile).toBeUndefined();
-
-    profile = await findOneByValue(
-      Profile,
-      { username: 'katrina560' },
-      asAdmin(),
-    );
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
-
-    profile = await findOneByValue(
-      Profile,
-      { username: 'clark218' },
-      asAdmin(),
-    );
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
-
-    profile = await findOneByValue(Profile, {}, asAdmin());
-    expect(removeDynamicData(Profile, profile)).toMatchSnapshot();
-
-    let boardMember;
-
-    boardMember = await findOneByValue(BoardMember, { board: 9999 }, asAdmin());
-    expect(boardMember).toBeUndefined();
-
-    boardMember = await findOneByValue(
-      BoardMember,
-      { board: 9, invitee: 7, inviter: 52 },
-      asAdmin(),
-    );
-    expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
-
-    boardMember = await findOneByValue(BoardMember, {}, asAdmin());
-    expect(removeDynamicData(BoardMember, boardMember)).toMatchSnapshot();
-  });
-
-  it('should utilize data loader', async () => {
-    const context = asAdmin();
-
-    const result = await Promise.all([
-      findOne(Profile, 10, {}, context),
-      findOne(Profile, 20, {}, context),
-      findOne(Profile, 30, {}, context),
-      findOne(Profile, 20, {}, context),
-      findOne(Profile, 10, {}, context),
-      findOneByValue(Profile, { username: 'katrina560' }, context),
-      findOneByValue(Profile, { username: 'clark218' }, context),
-      findOneByValue(Profile, { username: 'katrina560' }, context),
-    ]);
-
-    expect(removeListDynamicData(Profile, result)).toMatchSnapshot();
-  });
-
-  it('should return null when instance is not found', async () => {
-    const profile = await findOne(Profile, 10000, {}, asAdmin());
-    expect(profile).toBeNull();
+      expect(result).toMatchSnapshot();
+    });
   });
 });
