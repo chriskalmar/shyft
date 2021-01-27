8d72ef052b47fa75165cd6fe8c8e1090361f9ee0
Chris Kalmar <christian.kalmar@gmail.com>
2021-01-27 03:06:32 +0100

    extend mutation tests with graphql mutations

--- test/mutation.spec.ts
+++ test/mutation.spec.ts
@@ -1,5 +1,5 @@
 import './setupAndTearDown';
-import { mutate, findOneByValue } from './db';
+import { mutate, findOneByValue, testGraphql } from './db';
 
 import {
   asUser,
@@ -11,6 +11,7 @@ import {
 import { BoardMember } from './models/BoardMember';
 import { Board } from './models/Board';
 import { Book } from './models/Book';
+import { gql } from '../src/graphqlProtocol/util';
 
 describe('mutation', () => {
   describe('via connector', () => {
@@ -197,5 +198,339 @@ describe('mutation', () => {
         },
       );
     });
+  });
+
+  describe('via GrahpQL', () => {
+    it('perform create mutations', async () => {
+      const result = await testGraphql(
+        gql`
+          mutation joinBoardMember {
+            joinBoardMember(input: { boardMember: { board: 47 } }) {
+              boardMember {
+                id
+                board
+                inviter
+                invitee
+                state
+              }
+            }
+          }
+        `,
+        asUser(98),
+      );
+
+      expect(result).toMatchSnapshot();
+    });
+
+    it('perform create mutations with nested JSON attributes', async () => {
+      const result = await testGraphql(
+        gql`
+          mutation createBook {
+            createBook(
+              input: {
+                book: {
+                  title: "War and Peace"
+                  author: "Leo Tolstoy"
+                  reviews: [
+                    {
+                      reviewer: "John Connor"
+                      reviewText: "Couldn't stop reading"
+                      bookAttributes: [
+                        { attribute: "Year of publishing", value: "1867" }
+                        { attribute: "Pages", value: "1225" }
+                      ]
+                    }
+                  ]
+                }
+              }
+            ) {
+              book {
+                id
+                title
+                author
+                reviews {
+                  reviewer
+                  reviewText
+                  bookAttributes {
+                    attribute
+                    value
+                  }
+                }
+              }
+            }
+          }
+        `,
+        asUser(98),
+      );
+
+      expect(result).toMatchSnapshot();
+    });
+
+    it('perform update mutations', async () => {
+      const inviteResult = await testGraphql(
+        gql`
+          mutation inviteBoardMember {
+            inviteBoardMember(
+              input: { boardMember: { board: 48, invitee: 70 } }
+            ) {
+              boardMember {
+                id
+                board
+                inviter
+                invitee
+                state
+              }
+            }
+          }
+        `,
+        asUser(41),
+      );
+
+      expect(inviteResult).toMatchSnapshot('invite');
+
+      const inviteId = inviteResult.data.inviteBoardMember.boardMember.id;
+
+      const acceptResult = await testGraphql(
+        gql`
+          mutation acceptBoardMemberById($inviteId: ID!) {
+            acceptBoardMemberById(input: { id: $inviteId, boardMember: {} }) {
+              boardMember {
+                id
+                board
+                inviter
+                invitee
+                state
+              }
+            }
+          }
+        `,
+        asUser(70),
+        {
+          inviteId,
+        },
+      );
+
+      expect(acceptResult).toMatchSnapshot('accept');
+    });
+  });
+
+  it('perform update mutations with nested JSON attributes', async () => {
+    const book = await findOneByValue(
+      Book,
+      { author: 'Leo Tolstoy' },
+      asUser(99),
+    );
+
+    const result = await testGraphql(
+      gql`
+        mutation updateBookById($id: ID!) {
+          updateBookById(
+            input: {
+              id: $id
+              book: {
+                reviews: [
+                  {
+                    reviewer: "John Connor"
+                    reviewText: "Updated review"
+                    bookAttributes: [
+                      { attribute: "Year of publishing", value: "1867" }
+                      { attribute: "Pages", value: "1225" }
+                      { attribute: "Pages", value: "1225" }
+                      { attribute: "Genre", value: "Novel" }
+                    ]
+                  }
+                ]
+              }
+            }
+          ) {
+            book {
+              id
+              title
+              author
+              reviews {
+                reviewer
+                reviewText
+                bookAttributes {
+                  attribute
+                  value
+                }
+              }
+            }
+          }
+        }
+      `,
+      asUser(98),
+      {
+        id: book.id,
+      },
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('handle uniqueness constraints', async () => {
+    const firstResult = await testGraphql(
+      gql`
+        mutation buildBoard {
+          buildBoard(
+            input: { board: { name: "Next Board", isPrivate: false, vip: 2 } }
+          ) {
+            board {
+              id
+            }
+          }
+        }
+      `,
+      asUser(98),
+    );
+
+    expect(firstResult).toMatchSnapshot('first');
+
+    const secondResult = await testGraphql(
+      gql`
+        mutation buildBoard {
+          buildBoard(
+            input: { board: { name: "Next Board", isPrivate: false, vip: 2 } }
+          ) {
+            board {
+              id
+            }
+          }
+        }
+      `,
+      asUser(98),
+    );
+
+    expect(secondResult).toMatchSnapshot('second');
+  });
+
+  it('perform delete mutations', async () => {
+    const inviteResult = await testGraphql(
+      gql`
+        mutation inviteBoardMember {
+          inviteBoardMember(
+            input: { boardMember: { board: 48, invitee: 75 } }
+          ) {
+            boardMember {
+              id
+              board
+              inviter
+              invitee
+              state
+            }
+          }
+        }
+      `,
+      asUser(41),
+    );
+
+    expect(inviteResult).toMatchSnapshot('invite');
+
+    const inviteId = inviteResult.data.inviteBoardMember.boardMember.id;
+
+    const removeResult = await testGraphql(
+      gql`
+        mutation removeBoardMemberById($inviteId: ID!) {
+          removeBoardMemberById(input: { id: $inviteId }) {
+            deleteRowCount
+            id
+          }
+        }
+      `,
+      asUser(41),
+      {
+        inviteId,
+      },
+    );
+
+    expect(removeResult).toMatchSnapshot('remove');
+  });
+
+  it('fail mutation if fromState is not a match', async () => {
+    const inviteResult = await testGraphql(
+      gql`
+        mutation inviteBoardMember {
+          inviteBoardMember(
+            input: { boardMember: { board: 48, invitee: 72 } }
+          ) {
+            boardMember {
+              id
+              board
+              inviter
+              invitee
+              state
+            }
+          }
+        }
+      `,
+      asUser(41),
+    );
+
+    expect(inviteResult).toMatchSnapshot('invite');
+
+    const inviteId = inviteResult.data.inviteBoardMember.boardMember.id;
+
+    const firstAcceptResult = await testGraphql(
+      gql`
+        mutation acceptBoardMemberById($inviteId: ID!) {
+          acceptBoardMemberById(input: { id: $inviteId, boardMember: {} }) {
+            boardMember {
+              id
+              board
+              inviter
+              invitee
+              state
+            }
+          }
+        }
+      `,
+      asUser(72),
+      {
+        inviteId,
+      },
+    );
+
+    expect(firstAcceptResult).toMatchSnapshot('firstAccept');
+
+    const secondAcceptResult = await testGraphql(
+      gql`
+        mutation acceptBoardMemberById($inviteId: ID!) {
+          acceptBoardMemberById(input: { id: $inviteId, boardMember: {} }) {
+            boardMember {
+              id
+              board
+              inviter
+              invitee
+              state
+            }
+          }
+        }
+      `,
+      asUser(72),
+      {
+        inviteId,
+      },
+    );
+
+    expect(secondAcceptResult).toMatchSnapshot('secondAccept');
+  });
+
+  it('reject mutations if ID not found', async () => {
+    const result = await testGraphql(
+      gql`
+        mutation removeBoardMemberById($inviteId: ID!) {
+          removeBoardMemberById(input: { id: $inviteId }) {
+            deleteRowCount
+            id
+          }
+        }
+      `,
+      asUser(41),
+      {
+        inviteId: 99999,
+      },
+    );
+
+    expect(result).toMatchSnapshot();
   });
 });
