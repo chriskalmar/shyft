001de104fa88f3fc469760216cd310c28ccbd352
Chris Kalmar <christian.kalmar@gmail.com>
2021-01-30 11:38:31 +0100

    allow additional custom prop in Context

--- src/engine/context/Context.ts
+++ src/engine/context/Context.ts
@@ -1,6 +1,7 @@
 export interface Context {
   userId?: string | number;
   userRoles?: string[];
-  loaders: Record<string, unknown>;
+  loaders?: Record<string, unknown>;
   i18nLanguage?: string;
+  custom?: Record<string, unknown>;
 }
