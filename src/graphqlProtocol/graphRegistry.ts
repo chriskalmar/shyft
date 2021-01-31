86eb9989d3adc99e9de66400022a879dc64441bf
Chris Kalmar <christian.kalmar@gmail.com>
2021-01-31 02:47:00 +0100

    provide types to graph registry

--- src/graphqlProtocol/graphRegistry.ts
+++ src/graphqlProtocol/graphRegistry.ts
@@ -1,5 +1,22 @@
+import { GraphQLObjectType } from 'graphql';
+import { Action, Entity, ViewEntity } from '..';
+
+export interface GraphRegistryType {
+  types: {
+    [key: string]: {
+      entity: Entity | ViewEntity;
+      type: GraphQLObjectType;
+    };
+  };
+  actions: {
+    [key: string]: {
+      action: Action;
+    };
+  };
+}
+
 // collect object types, connections ... for each entity
-export const graphRegistry = {
+export const graphRegistry: GraphRegistryType = {
   types: {},
   actions: {},
 };
